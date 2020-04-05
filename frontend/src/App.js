e4051ac66527523e49494e1555e6577a7ecec687
Alexandre <alexandre.trapp@hotmail.com>
2020-04-05 19:55:48 -0300

    app, header, useState

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Header from './Header';
 
 function App() {
+    
+  const [counter, setCounter] = useState(0);
+
+  // Array [valor, funcaoDeAtualizacao]
+
+  function increment() {
+      setCounter(counter +1);
+  }
 
   return (
+    <div>
 
-    <h1>Hello teste</h1>
+        <Header>Contador: {counter}</Header>
+        <button onClick={increment}>Incrementar</button>
+    
+    </div>
   );
 }
 
