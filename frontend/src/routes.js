6588177666cd32d0fcf6dba136334e82faff8611
Alexandre <alexandre.trapp@hotmail.com>
2020-04-05 21:55:08 -0300

    route profile, js, css profiles

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,20 +3,21 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
 
     return (
 
         <BrowserRouter>
-
             <Switch>
 
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
 
-            </Switch>
+                <Route path="/profile" component={Profile} />
 
+            </Switch>
         </BrowserRouter>
     );
 }
